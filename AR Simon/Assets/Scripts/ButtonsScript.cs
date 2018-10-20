bf1e6478e940f6481b7608fc6e8cc0467c2c618e
Fenikkel <paufenollosa@gmail.com>
2018-10-20 12:18:40 +0200

    FirstVersion
    Virtual button and ray cast works!

---
+++ AR Simon/Assets/Scripts/ButtonsScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ButtonsScript : MonoBehaviour, IVirtualButtonEventHandler
+{
+
+    private GameObject redVB;
+    private GameObject redCube;
+    
+    void Start () {
+        redVB = GameObject.Find("RedButton");
+        redCube = GameObject.Find("RedCube");
+        redVB.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+
+    }
+
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        //throw new System.NotImplementedException();
+        redCube.SetActive(false);
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        //throw new System.NotImplementedException();
+        redCube.SetActive(true);
+
+    }
+}
